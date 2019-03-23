88fcec508db795018c7d6fceb61a62873957468d
Yakov Fat <YakovFat@yandex.ru>
2019-03-23 20:20:55 +0300

    gt

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,11 +1,31 @@
+import csv
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
 
 class InflationView(TemplateView):
     template_name = 'inflation.html'
 
     def get(self, request, *args, **kwargs):
-        # чтение csv-файла и заполнение контекста
         context = {}
+        with open('inflation_russia.csv', encoding='utf-8', newline='') as f:
+            read = csv.reader(f, delimiter=';')
+            list_csv = [i for i in read]
+            first_column = list_csv.pop(0)
+        list_info = []
+        list_1 = []
+        for lis in list_csv:
+            for i in lis:
+                if '.' in i:
+                    list_1.append(float(i))
+                elif i.isdigit():
+                    list_1.append(int(i))
+                elif len(i) == 0:
+                    list_1.append('-')
+            list_info.append(list_1)
+            list_1 = []
+        print(list_info)
+        context['first_column'] = first_column
+        context['info'] = list_info
         return render(request, self.template_name,
                       context)
--- dynamic-templates/task3/app/templatetags/news_filters.py
+++ dynamic-templates/task3/app/templatetags/news_filters.py
@@ -1,4 +1,6 @@
 from django import template
+from datetime import datetime
+import re
 
 
 register = template.Library()
@@ -6,17 +8,53 @@ register = template.Library()
 
 @register.filter
 def format_date(value):
-    # Ваш код
-    return value
+    now = datetime.now()
+    date_value = datetime.utcfromtimestamp(value)
+    result = now - date_value
+    second = result.total_seconds()
+    minutes = second / 60
+    if minutes < 10:
+        return 'только что'
+    elif 10 <= minutes <= 1440:
+        return f'{int(minutes/60)} часов назад'
+    else:
+        return date_value.date()
 
+
+@register.filter
+def format_score(value):
+    if value:
+        if value < -5:
+            return 'все плохо'
+        elif -5 <= value < 5:
+            return 'нейтрально'
+        elif value >= 5:
+            return 'хорошо'
+    else:
+        return 'рейтинг отсутствует'
 
 # необходимо добавить фильтр для поля `score`
 
 
 @register.filter
 def format_num_comments(value):
-    # Ваш код
-    return value
+    if value == 0:
+        return 'Оставьте отзыв'
+    elif 0 < value <= 50:
+        return value
+    else:
+        return '50+'
+
+
+@register.filter
+def format_selftext(value, count):
+    value_split = value.split(' ')
+    if len(value_split) > count*2:
+        value_split = value_split[:count] + ['...'] + value_split[-count:]
+        value = ' '.join(value_split)
+        return value
+    else:
+        return value
 
 
 
