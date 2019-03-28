31114fd38db7046e3075c1e0d3094f36ae86e55c
YaFat <yakovfat@gmail.com>
2019-03-28 12:06:28 +0300

    gt

--- databases/work_with_database/phones/views.py
+++ databases/work_with_database/phones/views.py
@@ -5,27 +5,13 @@ from .models import Phone
 def show_catalog(request):
     sort = request.GET.get('sort')
     if sort == 'name':
-        sort_name = Phone.objects.order_by("name")
-        return render(
-            request,
-            'catalog.html',
-            context={'sort_name': sort_name}
-        )
+        catalog = Phone.objects.order_by("name")
     elif sort == 'min_price':
-        sort_name = Phone.objects.order_by("price")
-        return render(
-            request,
-            'catalog.html',
-            context={'sort_name': sort_name}
-        )
+        catalog = Phone.objects.order_by("price")
     elif sort == 'max_price':
-        sort_name = Phone.objects.order_by("-price")
-        return render(
-            request,
-            'catalog.html',
-            context={'sort_name': sort_name}
-        )
-    catalog = Phone.objects.all()
+        catalog = Phone.objects.order_by("-price")
+    else:
+        catalog = Phone.objects.all()
     return render(
         request,
         'catalog.html',
