98aa7da4126287a4c0d4cd24a15113bd3fd70416
YakovFat <yakovfat94@gmail.com>
2019-04-10 23:23:47 +0300

    gt

--- site-personalization/paid_content/articles/models.py
+++ site-personalization/paid_content/articles/models.py
@@ -2,9 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class Profile(models.Model):
-    pass
-
-
 class Article(models.Model):
-    pass
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    pay_article = models.BooleanField(default=False)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    has_subcription = models.BooleanField(default=False)
--- site-personalization/paid_content/articles/views.py
+++ site-personalization/paid_content/articles/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
-
+from .models import Article, Profile
 
 def show_articles(request):
+    art = Article.objects.all()
     return render(
         request,
-        'articles.html'
+        'articles.html',
+        {'art': art}
     )
 
 
 def show_article(request, id):
+    obj = Article.objects.get(id=id)
+    user = request.user
+    prof = Profile.objects.get(user=user)
+    prof_1 = prof.has_subcription
+    print(prof_1)
+    if obj.pay_article:
+        context = {'title': obj.title, 'limited': 'Статья платная, пожалуйста, подпишитесь'}
+    else:
+        context = {'title': obj.title, 'body': obj.text}
     return render(
         request,
-        'article.html'
+        'article.html',
+        context
     )
