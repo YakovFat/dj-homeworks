76a28153a774d26df721afc367aefdf98d545781
Yakov Fat <YakovFat@yandex.ru>
2019-04-19 22:43:31 +0300

    gt

--- creating-project/cli_and_doc/cli_and_docs/cli_and_docs/settings.py
+++ creating-project/cli_and_doc/cli_and_docs/cli_and_docs/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dj_admin'
 ]
 
 MIDDLEWARE = [
@@ -88,9 +89,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    # },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
